b3cb08eec0e7f7fa34c9f36789955bf5dca5d69d
Girish <gkurkut1@binghamton.edu>
2018-06-26 11:03:11 -0700

    Add project files.

---
+++ BouncingBall/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.OvalShape1 = New Microsoft.VisualBasic.PowerPacks.OvalShape()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.OvalShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(628, 286)
+        Me.ShapeContainer1.TabIndex = 0
+        Me.ShapeContainer1.TabStop = False
+        '
+        'OvalShape1
+        '
+        Me.OvalShape1.BackColor = System.Drawing.Color.Blue
+        Me.OvalShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque
+        Me.OvalShape1.Location = New System.Drawing.Point(133, 90)
+        Me.OvalShape1.Name = "OvalShape1"
+        Me.OvalShape1.Size = New System.Drawing.Size(43, 42)
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(491, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(106, 25)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(491, 80)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(106, 24)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button3.Location = New System.Drawing.Point(491, 127)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(106, 24)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Speed Up "
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button4.Location = New System.Drawing.Point(491, 168)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(106, 26)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Speed Down"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(491, 230)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(491, 211)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Select Angle"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(628, 286)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ShapeContainer1 As PowerPacks.ShapeContainer
+    Friend WithEvents OvalShape1 As PowerPacks.OvalShape
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ BouncingBall/Form1.vb
@@ -0,0 +1,98 @@
+﻿'Option Strict On
+'Option Explicit On
+Public Class Form1
+    Dim MoveRight As Boolean
+    Dim MoveUp As Boolean
+    Dim Speed As Integer = 1
+    Dim var As String
+
+    'Dim Left As Integer = Integer.Parse(OvalShape1.Left)
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        var = ComboBox1.Text
+        If var = "Angle 1" Then
+            If MoveRight = True Then
+                OvalShape1.Left = OvalShape1.Left + Speed
+            Else
+                OvalShape1.Left = OvalShape1.Left - Speed
+            End If
+
+            If MoveUp = True Then
+                OvalShape1.Top = OvalShape1.Top - Speed
+            Else
+                OvalShape1.Top = OvalShape1.Top + Speed
+            End If
+        End If
+
+        If var = "Angle 2" Then
+            If MoveRight = True Then
+                OvalShape1.Left = OvalShape1.Left + Speed + 2
+            Else
+                OvalShape1.Left = OvalShape1.Left - Speed - 2
+            End If
+
+            
+        End If
+
+        If var = "Angle 3" Then
+            If MoveRight = True Then
+                OvalShape1.Left = OvalShape1.Left + Speed + 3
+            Else
+                OvalShape1.Left = OvalShape1.Left - Speed - 3
+            End If
+
+            If MoveUp = True Then
+                OvalShape1.Top = OvalShape1.Top - Speed + 3
+            Else
+                OvalShape1.Top = OvalShape1.Top + Speed - 3
+            End If
+        End If
+
+        If OvalShape1.Left < Me.ClientRectangle.Left Then
+            MoveRight = True
+        End If
+        If (OvalShape1.Left + OvalShape1.Width + 150) >= Me.ClientRectangle.Right Then
+            MoveRight = False
+        End If
+        If OvalShape1.Top <= Me.ClientRectangle.Top Then
+            MoveUp = False
+        End If
+        If (OvalShape1.Top + OvalShape1.Height) >= Me.ClientRectangle.Bottom Then
+            MoveUp = True
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If (Speed < 10) Then
+
+            Speed = Speed + 1
+
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If (Speed > 4) Then
+
+            Speed = Speed - 1
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Items.Add("Angle 1")
+        ComboBox1.Items.Add("Angle 2")
+        ComboBox1.Items.Add("Angle 3")
+        ComboBox1.SelectedItem = "Angle 1"
+    End Sub
+
+
+End Class
